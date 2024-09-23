9889839cee6ec86f4da23b7ed22e4b317d4817ab
Eduardo Moraes <eduardo.moraes@aquasoft.com.br>
2024-09-23 19:07:53 -0300

    Separação de Modais e melhoria em layout.

---
+++ WebControleFinanceiro/Pages/Shared/_Modals.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebControleFinanceiro.Pages.Shared
+{
+    public class _ModalsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
